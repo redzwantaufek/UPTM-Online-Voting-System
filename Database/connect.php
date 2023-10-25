97cfd1a6e77ab0e7a20f0e3468bb4a0ed2531a12
redzwantaufek <redzwantaufek2@gmail.com>
2023-10-25 22:37:13 +0800

    delete the end session

--- Database/connect.php
+++ Database/connect.php
@@ -11,9 +11,4 @@ $conn = new mysqli($host, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
-echo "Connected successfully";
-
-// Close connection
-$conn->close();
 ?>
