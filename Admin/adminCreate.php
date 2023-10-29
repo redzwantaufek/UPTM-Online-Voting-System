4ea6132f3d5bf9c9c746811de71f624a2b355b39
redzwantaufek <redzwantaufek2@gmail.com>
2023-10-29 13:52:59 +0800

    minor changes

--- Admin/adminCreate.php
+++ Admin/adminCreate.php
@@ -162,6 +162,7 @@
                         <h6 class="collapse-header">MENU</h6>
                         <a class="collapse-item" href="electionView.php">Election View</a>
                         <a class="collapse-item" href="electionSet.php">Election Set Up</a>
+                        <a class="collapse-item" href="annSet.php">Election Announcement</a>
                     </div>
                 </div>
             </li>
