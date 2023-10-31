d488929547ee8cef51b792a104a15e5c8461d3c8
redzwantaufek <redzwantaufek2@gmail.com>
2023-10-31 22:33:44 +0800

    write some code

--- Admin/result.php
+++ Admin/result.php
@@ -1,3 +1,33 @@
+<?php
+    include '../Database/connect.php';
+    session_start();
+
+    // Check if user is logged in
+    if (!isset($_SESSION['admin_id'])) {
+        // If not, redirect to login page
+        header('Location: ../login.php');
+        exit();
+    }
+
+    // Query to select the admin details from the database using the admin ID from the session
+    $sql = "SELECT * FROM admin WHERE adminID = '".$_SESSION['admin_id']."'";
+    // Execute the query
+    $result = $conn->query($sql);
+
+    // If the query returns more than 0 rows, fetch the admin details
+    if ($result->num_rows > 0) {
+        $admin = $result->fetch_assoc();
+        // Get the image path from the database
+        $imagePath = $admin['pic'];
+    } else {
+        // If no admin details are found, display an error message and exit the script
+        echo "No admin found";
+        exit();
+    }
+
+   
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -30,178 +60,182 @@
          <!-- _______________________________________________Sidebar_______________________________________________ -->
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-            <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <div class="sidebar-brand-icon">
-                    <i class="fa-solid fa-user-tie"></i>
-                </div>
-                <div class="sidebar-brand-text mx-3">Dashboard</div>
-            </a>
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+        <div class="sidebar-brand-icon">
+            <i class="fa-solid fa-square-poll-vertical"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Dashboard</div>
+    </a>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider my-0">
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
 
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item">
-                <a class="nav-link" href="index.html">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
-            </li>
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item">
+        <a class="nav-link" href="index.php">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider">
+    <!-- Divider -->
+    <hr class="sidebar-divider">
 
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                OPTIONS
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        OPTIONS
+    </div>
+
+    <!-- Nav Item - Admin Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+            aria-expanded="true" aria-controls="collapseTwo">
+            <i class="fas fa-fw fa-cog"></i>
+            <span>Administration</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">MENU</h6>
+                <a class="collapse-item" href="adminProfiles.php">View Profile</a>
+                <a class="collapse-item" href="adminCreate.php">Create Admin</a>
+                <a class="collapse-item" href="adminList.php">List Admin</a>
             </div>
+        </div>
+    </li>
 
-            <!-- Nav Item - Admin Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-                    aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-cog"></i>
-                    <span>Administration</span>
-                </a>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">MENU</h6>
-                        <a class="collapse-item" href="adminProfiles.html">View Profile</a>
-                        <a class="collapse-item" href="adminEdit.html">Edit Admin</a>
-                        <a class="collapse-item" href="adminCreate.html">Create Admin</a>
-                    </div>
-                </div>
-            </li>
-
-            <!-- Nav Item - Candidates Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-                    aria-expanded="true" aria-controls="collapseUtilities">
-                    <i class="fa-solid fa-users"></i>
-                    <span>Candidate</span>
-                </a>
-                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-                    data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">MENU</h6>
-                        <a class="collapse-item" href="candidateCreate.html">Add Candidates</a>
-                        <a class="collapse-item" href="candidateView.html">View Candidates</a>
-                        <a class="collapse-item" href="candidateEdit.html">Edit Candidates</a>
-                    </div>
-                </div>
-            </li>
-
-            <!-- Nav Item - Election Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-                    aria-expanded="true" aria-controls="collapsePages">
-                    <i class="fa-solid fa-check-to-slot"></i>
-                    <span>Election</span>
-                </a>
-                <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">MENU</h6>
-                        <a class="collapse-item" href="electionView.html">Election View</a>
-                        <a class="collapse-item" href="electionSet.html">Election Set Up</a>
-                    </div>
-                </div>
-            </li>
-
-            <!-- Nav Item - Student Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseStudent"
-                    aria-expanded="true" aria-controls="collapsePages">
-                    <i class="fa-solid fa-person"></i>
-                    <span>Student</span>
-                </a>
-                <div id="collapseStudent" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">MENU</h6>
-                        <a class="collapse-item" href="studentView.html">View Student Profile</a>
-                        <a class="collapse-item" href="studentEdit.html">Edit Student</a>
-                        <a class="collapse-item" href="studentCreate.html">Create Student</a>
-                    </div>
-                </div>
-            </li>
-
-            <!-- Nav Item - Result -->
-            <li class="nav-item  active">
-                <a class="nav-link" href="result.html">
-                    <i class="fa-solid fa-chart-simple"></i>
-                    <span>Election Result</span></a>
-            </li>
-
-            <!-- Nav Item - Student attendance -->
-            <li class="nav-item">
-                <a class="nav-link" href="attendance.html">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Student Attendance</span></a>
-            </li>
-            
-            <!-- Divider -->
-            <hr class="sidebar-divider d-none d-md-block">
-
-            <!-- Sidebar Toggler (Sidebar) -->
-            <div class="text-center d-none d-md-inline">
-                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    <!-- Nav Item - Candidates Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+            aria-expanded="true" aria-controls="collapseUtilities">
+            <i class="fa-solid fa-users"></i>
+            <span>Candidate</span>
+        </a>
+        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+            data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">MENU</h6>
+                <a class="collapse-item" href="candidateCreate.php">Verify Candidates</a>
+                <a class="collapse-item" href="candidateView.php">View Candidates</a>
             </div>
+        </div>
+    </li>
 
-        </ul>
-        <!-- _______________________________________________End of Sidebar___________________________________________________________ -->
-        
+    <!-- Nav Item - Election Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+            aria-expanded="true" aria-controls="collapsePages">
+            <i class="fa-solid fa-check-to-slot"></i>
+            <span>Election</span>
+        </a>
+        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">MENU</h6>
+                <a class="collapse-item" href="electionView.php">Election View</a>
+                <a class="collapse-item" href="electionSet.php">Election Set Up</a>
+                <a class="collapse-item" href="annSet.php">Election Announcement</a>
+            </div>
+        </div>
+    </li>
 
-        <!-- Content Wrapper -->
-        <div id="content-wrapper" class="d-flex flex-column">
+    <!-- Nav Item - Student Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseStudent"
+            aria-expanded="true" aria-controls="collapsePages">
+            <i class="fa-solid fa-person"></i>
+            <span>Student</span>
+        </a>
+        <div id="collapseStudent" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">MENU</h6>
+                <a class="collapse-item" href="studentView.php">View Student Profile</a>
+                <a class="collapse-item" href="studentCreate.php">Create Student</a>
+            </div>
+        </div>
+    </li>
 
-            <!-- Main Content -->
-            <div id="content">
+    <!-- Nav Item - Result -->
+    <li class="nav-item">
+        <a class="nav-link" href="result.php">
+            <i class="fa-solid fa-chart-simple"></i>
+            <span>Election Result</span></a>
+    </li>
 
-                <!-- Topbar -->
-                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+    <!-- Nav Item - Student attendance -->
+    <li class="nav-item">
+        <a class="nav-link" href="attendance.php">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Student Attendance</span></a>
+    </li>
 
-                    <!-- Sidebar Toggle (Topbar) -->
-                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                        <i class="fa fa-bars"></i>
-                    </button>
+    <!-- Nav Item - Result -->
+    <li class="nav-item active">
+        <a class="nav-link" href="about.php">
+            <i class="fas fa-fw fa-cog"></i>
+            <span>About</span></a>
+    </li>
 
-                    <!-- UPTM Logo -->
-                        <div class="navbar-brand" href="#">
-                            <img src="img/uptm.jpg" alt="" class="img-fluid logo-img" style="max-width: 100px; max-height: 100px;">
-                        </div>
 
-                                
 
-                    <!-- Topbar Navbar -->
-                    <ul class="navbar-nav ml-auto">
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+    </ul>
+    <!-- _______________________________________________End of Sidebar___________________________________________________________ -->
+
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+    <!-- Main Content -->
+    <div id="content">
+
+        <!------------------------------------ Topbar ------------------------------------------------------->
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+            <!-- Sidebar Toggle (Topbar) -->
+            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                <i class="fa fa-bars"></i>
+            </button>
+
+            <!-- UPTM Logo -->
+                <div class="navbar-brand" href="#">
+                    <img src="img/uptm.jpg" alt="" class="img-fluid logo-img" style="max-width: 100px; max-height: 100px;">
+                </div>            
+
+            <!-- Topbar Navbar -->
+            <ul class="navbar-nav ml-auto">
+                
+                <!-- Nav Item - User Information -->
+                <li class="nav-item dropdown no-arrow">
+                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $_SESSION['user_name']; ?></span>
+                        <img src="<?php echo $imagePath; ?>" class="img-profile rounded-circle img-fluid" title="profile images" 
+                        style="max-width: 200px;" onerror="this.onerror=null; this.src='../img/no_profile.webp'">
+                    </a>
+                    <!-- Dropdown - User Information -->
+                    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                        aria-labelledby="userDropdown">
+                        <a class="dropdown-item" href="adminProfiles.php">
+                            <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Profile
+                        </a>
                         
-                        <!-- Nav Item - User Information -->
-                        <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Ali bin Abu</span>
-                                <img class="img-profile rounded-circle" title="profile images"
-                                    src="img/undraw_profile.svg">
-                            </a>
-                            <!-- Dropdown - User Information -->
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="adminProfiles.html">
-                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Profile
-                                </a>
-                                
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="" data-toggle="modal" data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Logout
-                                </a>
-                            </div>
-                        </li>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="logout.php">
+                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Logout
+                        </a>
+                    </div>
+                </li>
+            </ul>
 
-                    </ul>
-
-                </nav>
-                <!-- _______________________________________End of Topbar__________________________________________________________ -->
+        </nav>
+        <!-- _______________________________________End of Topbar__________________________________________________________ -->
 
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
