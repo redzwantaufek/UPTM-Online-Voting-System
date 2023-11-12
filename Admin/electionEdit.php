653699dda14aae925b38c83c731b207b6a727009
redzwantaufek <redzwantaufek2@gmail.com>
2023-11-12 16:26:55 +0800

    change some title

--- Admin/electionEdit.php
+++ Admin/electionEdit.php
@@ -281,7 +281,7 @@
 
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Admin Profile</h1>
+                        <h1 class="h3 mb-0 text-gray-800">Edit Election</h1>
                     </div>
 
                     <!-- Content Row -->
